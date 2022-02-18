0f54a584563e54a8357e5ded498f0759adc7ae87
Diego Gonzalez <degonzalez6@gmail.com>
2022-02-17 23:46:49 -0300

    Primer Commit - Creación del proyecto y conexión a la base de datos

---
+++ src/main/java/com/sofka/ej1ApiRest/Sofka/SofkaEj1ApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.ej1ApiRest.Sofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaEj1ApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkaEj1ApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/ej1ApiRest/Sofka/SofkaEj1ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.ej1ApiRest.Sofka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkaEj1ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
